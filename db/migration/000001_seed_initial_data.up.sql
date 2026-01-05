96fff0de03d471ec4c94ac71411b4c3802721981
bitcat <kurohaj96@gmail.com>
2026-01-06 07:50:35 +0900

    fix: migration query

--- db/migration/000001_seed_initial_data.up.sql
+++ db/migration/000001_seed_initial_data.up.sql
@@ -1,12 +1,12 @@
 -- Insert Company
-INSERT INTO "Company" (id, name, updated_at, created_at)
+INSERT INTO "Company" (id, name)
 VALUES (
     '0f036454-617a-493c-ae1f-f05efcbbb330',
     '0UTL1ER株式会社'
 );
 
 -- Insert User
-INSERT INTO "User" (id, company_id, name, role, updated_at, created_at)
+INSERT INTO "User" (id, company_id, name, role)
 VALUES (
     'jlbeL9zvGhMB5tV2CLy8MCy3iYn2',
     '0f036454-617a-493c-ae1f-f05efcbbb330',
